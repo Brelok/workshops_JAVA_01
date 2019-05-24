29d1bfe013b9afb2c01de5868713f4b3d7b4c702
Paweł Pradelok <code2pradelok@gmail.com>
2019-05-24 13:42:52 +0200

    GuessNUmber

--- src/Brelok/workshops/GuessNumber.java
+++ src/Brelok/workshops/GuessNumber.java
@@ -1,8 +1,36 @@
 package Brelok.workshops;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessNumber {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Random random = new Random();
+
+        int guess = random.nextInt(101) + 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Zagraj ze mna! Zgadnij liczbę w zakresie od 1 do 100!: ");
+
+        int check = 0;
+        while (true) {
+            while (!scanner.hasNextInt()) {
+                scanner.nextLine();
+                System.out.print("To nie jest liczba, nie oszukuj!: ");
+            }
+            check = scanner.nextInt();
+
+            if (check < guess) {
+                System.out.print("Daj więcej: ");
+
+            } else if (check > guess) {
+                System.out.print("Daj mniej!: ");
+
+            } else {
+                System.out.println("Brawo! Zgadłeś!");
+                break;
+            }
+        }
     }
 }
